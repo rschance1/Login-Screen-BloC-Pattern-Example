9513edf94b15a52959997db1fc5b2e63f5eb1f72
rschance1 <richardchance1980@gmal.com>
2019-02-14 14:09:15 -0600

    Loading project

---
+++ lib/src/blocs/validators.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+class Validators {
+  get validateEmail => StreamTransformer<String, String>.fromHandlers(handleData: (email, sink) {
+    if (email.contains('@')) {
+      sink.add(email);
+    } else {
+      sink.addError('Enter a valid email');
+    }
+  });
+
+  get validatePassword => StreamTransformer<String, String>.fromHandlers(
+      handleData: (password, sink) {
+    if (password.length > 3) {
+      sink.add(password);
+    } else {
+      sink.addError('Password must be 4 characters');
+    }
+  });
+}
